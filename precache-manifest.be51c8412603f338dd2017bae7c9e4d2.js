d8ef938c5e4b8302b0a65f696a5bffe20f9345ee
Kashishdwn <kashishdwn@gmail.com>
2020-05-12 03:37:23 +0530

    Updates

---
+++ precache-manifest.be51c8412603f338dd2017bae7c9e4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7111090f9dea1fbb87bd1db3ba1e4349",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "3a8af9ae3056d2c3bae5",
+    "url": "/Todo-List/static/css/main.2fa306fd.chunk.css"
+  },
+  {
+    "revision": "e7896449d0c81f2eda3f",
+    "url": "/Todo-List/static/js/2.3b69b362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List/static/js/2.3b69b362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8af9ae3056d2c3bae5",
+    "url": "/Todo-List/static/js/main.f8277f14.chunk.js"
+  },
+  {
+    "revision": "171d1a8d330ac7ad9113",
+    "url": "/Todo-List/static/js/runtime-main.d0310e8b.js"
+  }
+]);
